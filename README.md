2d4503107459806e0a2c903acbea5706f0b206cc
Mustafa Ramadhan <mustafa@bigraf.com>
2012-09-19 20:33:41 +0700

    Modified README.md and add script for creating kloxo-custom.repo

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# Kloxo
+# Kloxo-MR
 Rpm packages for Kloxo-MR (Kloxo fork by Mustafa Ramadhan). Also possible to be using for Kloxo official release (6.1.12+).
 
 # How to use this repo
 
     cd /etc/yum.repos.d
-	wget https://github.com/mustafaramadhan/kloxo/raw/rpms/kloxo-github.repo
+	wget https://github.com/mustafaramadhan/kloxo/raw/rpms-release/create-kloxocustomrepo.sh
+	sh ./create-kloxocustomrepo.sh
 	yum clean all
---
+++ create-kloxocustomrepo.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+# release on Kloxo 6.2.0
+# by mustafa.ramadhan@lxcenter.org
+
+RELEASEVER=$(rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release))
+
+echo "- Creating custom-repo inside '/home/rpms' "
+echo "     with kloxo-custom.repo inside '/etc/yum.repos.d'"
+
+echo "[kloxo-release-noarch]
+name=kloxo-release-noarch
+baseurl=https://github.com/mustafaramadhan/kloxo/raw/rpms-release/centos$RELEASEVER/noarch/
+enabled=1
+gpgcheck=0
+
+[kloxo-release-arch]
+name=kloxo-release-$basearch
+baseurl=https://github.com/mustafaramadhan/kloxo/raw/rpms-release/centos$RELEASEVER/$basearch/
+enabled=1
+gpgcheck=0
+
+[kloxo-testing-noarch]
+name=kloxo-testing-noarch
+baseurl=https://github.com/mustafaramadhan/kloxo/raw/rpms-testing/centos$RELEASEVER/noarch/
+enabled=0
+gpgcheck=0
+
+[kloxo-testing-arch]
+name=kloxo-testing-$basearch
+baseurl=https://github.com/mustafaramadhan/kloxo/raw/rpms-testing/centos$RELEASEVER/$basearch/
+enabled=0
+gpgcheck=0
+
+# ==================================
+
+[kloxo-centalt]
+name=kloxo-centalt - $basearch
+baseurl=http://centos.alt.ru/repository/centos/$RELEASEVER/$basearch/
+enabled=1
+gpgcheck=0
+exclude=openssh*
+
+# ==================================
+
+[kloxo-ius]
+name=kloxo - IUS Community Packages for Enterprise Linux $RELEASEVER - $basearch
+baseurl=http://dl.iuscommunity.org/pub/ius/stable/Redhat/$RELEASEVER/$basearch
+mirrorlist=http://dmirr.iuscommunity.org/mirrorlist/?repo=ius-el$RELEASEVER&arch=$basearch
+enabled=1
+gpgcheck=0
+
+# ==================================
+
+[kloxo-epel]
+name=kloxo - Extra Packages for Enterprise Linux $RELEASEVER - $basearch
+baseurl=http://download.fedoraproject.org/pub/epel/$RELEASEVER/$basearch
+mirrorlist=http://mirrors.fedoraproject.org/mirrorlist?repo=epel-$RELEASEVER&arch=$basearch
+enabled=1
+gpgcheck=0" > /etc/yum.repos.d/kloxo-custom.repo
